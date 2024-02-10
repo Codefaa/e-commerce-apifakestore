d12616fa8a1ce42af533c1c9febdd5b6babe7402
Codefaa <facundogarcia1999@outlook.com>
2024-02-10 04:18:47 -0300

    cambiooss

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <CartProvider>
         <FavoritosProvider>
 
-          <BrowserRouter basename="https://codefaa.github.io/e-commerce-apifakestore/">
+          <BrowserRouter basename="/e-commerce-apifakestore/">
 
             <Encabezado onSearch={handleSearch} busqueda={busqueda} /> {/* Pasa los términos de búsqueda al Encabezado */}
 
